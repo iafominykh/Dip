34170b5714fdb3ca1dc6baf3031bf373370df4d6
iafominykh <iafominykh@yandex.ru>
2023-11-10 21:50:32 +0300

    com5

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from courses.models import Course, Lesson
+from courses.models import Course, Lesson, Test, Question, Answer
 
 
 @admin.register(Course)
@@ -14,3 +14,16 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'preview', 'description', 'lesson_url', 'course', 'owner')
 
 
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lesson', 'test_title', 'description', 'is_public', 'owner')
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'test', 'question_text')
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'answer_text', 'is_correct')
