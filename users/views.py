7054c5337d4e2a6d4a1ec2aee2ba52b8914793c8
iafominykh <iafominykh@yandex.ru>
2023-11-11 10:48:16 +0300

    com8

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+
 from rest_framework import viewsets, generics
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 from users.models import User
 from users.serializers import UserSerializer
